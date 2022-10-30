7250ad749d03f260e643dccddeaa272830df7245
Elton Mozart <elton.silva.ext@dasa.com.br>
2022-10-29 23:08:59 -0300

    :sparkles: Nova tela de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,106 @@
-import { useRouter } from "next/router";
+import { useState } from "react";
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import axios from "axios";
+import Stripe from "stripe";
+import { stripe } from "../../lib/stripe";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
+import Head from "next/head";
 
-export default function ProductItem() {
-  const { query } = useRouter();
-  return <h1>{`Product ${query.id}`}</h1>;
+interface ProductItemProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+    defaultPriceId: string;
+  };
 }
+
+export default function ProductItem({ product }: ProductItemProps) {
+  const [isCreateingCheckout, setIsCreatingCheckout] = useState(false);
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckout(true);
+      const response = await axios.post("/api/checkout", {
+        priceId: product.defaultPriceId,
+      });
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+    } catch (error) {
+      setIsCreatingCheckout(false);
+      alert("Erro ao comprar o produto");
+    }
+  }
+
+  console.log(product.defaultPriceId);
+
+  return (
+    <>
+      <Head>
+        <title>Ignite Shop | {product.name}</title>
+      </Head>
+
+      <ProductContainer>
+        <ImageContainer>
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            width={520}
+            height={480}
+          />
+        </ImageContainer>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+          <p>{product.description}</p>
+          <button onClick={handleBuyProduct} disabled={isCreateingCheckout}>
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
+  );
+}
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "prod_Mhjdw3RmPkbL5r" } }],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id;
+  const product = await stripe.products.retrieve(productId as string, {
+    expand: ["default_price"],
+  });
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount / 100),
+        description: product.description,
+        defaultPriceId: price.id,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hours
+  };
+};
